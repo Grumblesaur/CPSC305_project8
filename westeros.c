2a0d6c9b1552e7adfb22633c2211f454264a48a8
James Murphy <jmurphy4@mail.umw.edu>
2015-04-19 22:42:18 -0400

    Worked on place_order() function.

--- westeros.c
+++ westeros.c
@@ -2,10 +2,44 @@
 #include <string.h>
 #include "westeros.h"
 
+// really, this whole thing is a constructor for the "struct order" type
 struct order * place_order(struct customer *customer, char *description,
 	int quant, float unit_price_in_gold, int *num_orders) {
 	
-	// do stuff here
+	// allocate memory for this order
+	struct order *this_order = (struct order*) malloc(sizeof(struct order));
+	
+	// track unique order numbers in the data segment for permanence
+	static int order_num = 1;
+	
+	// point this order at the customer we received
+	this_order->cust = customer;
+	
+	// copy the item description
+	strcpy(this_order->desc, description);
+
+	// copy the number of items
+	this_order->quantity = quant;
+	
+	// copy the price
+	this_order->unit_price_in_gold = unit_price_in_gold;
+	
+	// give the current order an ID number, then increment for next order
+	this_order->order_num = order_num++;
+	
+	// determine whether this is a rush order
+	if (strcmp(customer.family, "Lannister") == 0) {
+		this_order->rush = 1;
+	} else {
+		this_order->rush = 0;
+	}
+
+	// we don't know where this thing is gonna point to later, make it NULL	
+	this_order->next = NULL;
+	
+	//TODO: figure out what to do with the num_orders argument
+	// -- this will probably require looking at Stephen's assignment page	
+	
 }
 
 int fulfill_order(char *customer_name, int order_num, float *revenue) {
