b3b888159e50ae2d6e9f6fd13746bde77eb605f3
James Murphy <jmurphy4@mail.umw.edu>
2015-04-20 16:36:10 -0400

    Refactored comments and minor pieces of code.

--- westeros.c
+++ westeros.c
@@ -8,25 +8,16 @@ struct order * place_order(struct customer *customer, char *description,
 	int quant, float unit_price_in_gold, int *num_orders) {
 	
 	// allocate memory for this order
-	struct order *this_order;
-	this_order = (struct order*) malloc(sizeof(struct order));
+	struct order *this_order = (struct order*) malloc(sizeof(struct order));
 	
 	// track unique order numbers in the data segment for permanence
 	static int order_num = 1;
-	
-	// point this order at the customer we received
+
+	// dumbly copy-initialize these fields	
 	this_order->cust = customer;
-	
-	// copy the item description
 	strcpy(this_order->desc, description);
-	
-	// copy the number of items
 	this_order->quantity = quant;
-	
-	// copy the price
 	this_order->unit_price_in_gold = unit_price_in_gold;
-	
-	// give the current order an ID number, then increment for next order
 	this_order->order_num = order_num++;
 	
 	// determine whether this is a rush order
@@ -38,31 +29,24 @@ struct order * place_order(struct customer *customer, char *description,
 	
 	this_order->next = NULL;
 	
-	// find out how many orders the customer currently has
-	static int counter = 0;
+	// code for adding to a linked list and counting order numbers
+	static int counter = 0; // not sure if correct, but it works for now.
 	struct order * curr;
 	if (customer->orders != NULL) {
 		curr = customer->orders;
-		
-		// if the order list isn't empty, count 
 		while(curr->next != NULL) {
 			++counter;
 			curr = curr->next;
 		}
-		
-		// add new order to linked list
 		curr->next = this_order;
 		++counter;
 	} else {
-		// do this when the list is empty
 		customer->orders = this_order;
 		counter = 1;
 	}
 	
 	// add one more for THIS order, increment value pointed at by num_orders
 	*num_orders = counter;
-	printf("Current number of orders is %d.\n", *num_orders);
-	
 	return this_order;		
 }
 
