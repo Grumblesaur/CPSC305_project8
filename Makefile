c34489205d3756c1a2b3679386712ca60c318cbe
James Murphy <jmurphy4@mail.umw.edu>
2015-04-21 16:32:55 -0400

    Tried a few things; no success; code DOES compile.

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
-westeros: westeros.c stephen.c test.c
-	gcc -o westeros westeros.c stephen.c test.c
+westeros: westeros.o stephen.o test.o
+	gcc -o westeros westeros.o stephen.o test.o
+
+westeros.o: westeros.c westeros.h
+	gcc -c westeros.c
+
+stephen.o: stephen.c westeros.h
+	gcc -c stephen.c
+
+test.o: test.c westeros.h
+	gcc -c test.c
 
 run: westeros
 	./westeros ec=false
--- westeros.c
+++ westeros.c
@@ -45,8 +45,6 @@ struct order * place_order(struct customer *customer, char *description,
 		counter = 1;
 	}
 	
-	printf("%s has placed order %d.\n", customer->name, order_num - 1);
-	
 	// add one more for THIS order, increment value pointed at by num_orders
 	*num_orders = counter;
 	return this_order;		
@@ -55,81 +53,60 @@ struct order * place_order(struct customer *customer, char *description,
 int fulfill_order(char *customer_name, int order_num, float *revenue) {
 	// use Stephen's function to pull the customer from the database
 	struct customer * cust = lookup_customer(customer_name);
-	
 	struct order * curr = cust->orders;
-	
-	signed char order_exists = 0; // this is a boolean; 0 : false, -1 : true
+	int order_exists = 0; // this is a boolean; 0 : false, -1 : true
 	float cash = 0; // we'll write this to the data pointed at by revenue
-	
-	printf("%s wants order #%d now.\n", customer_name, order_num);
 	
 	// make sure the customer has placed orders
 	if (curr != NULL) {
-		printf("step 1\n");
-		if (curr->order_num != order_num) {
-			printf("step 2\n");
-			while(curr->next != NULL) {
+		if (curr->order_num == order_num) {
+				order_exists = -1;
+				cash = curr->unit_price_in_gold * curr->quantity;
+		} else {
+			while (curr->next != NULL) {
 				if (curr->order_num == order_num) {
-					printf("Found order #%d by looping\n", order_num);
 					order_exists = -1;
 					cash = curr->unit_price_in_gold * curr->quantity;
 					break;
 				}
 				curr = curr->next;
-				printf("iterated while loop\n");
 			}
-		} else {
-			printf("step 3\n");
-			order_exists = -1;
-			cash = curr->unit_price_in_gold * curr->quantity;
 		}
-	} else {
+	} else { // if no orders, bail out
 		*revenue = 0;
-		printf("Bailed out.\n");
 		return 0;
 	}
 	
 	if (order_exists == 0) {
 		*revenue = 0;
-		printf("Bailed out.\n");
 		return 0;
 	}
 	
 	struct order * temp = NULL;
-	// if the order was at the front of the line
-	if (cust->orders->order_num == order_num) {
-		printf("Deleting order #%d.\n", order_num);
+
+	// reset curr pointer
+	curr = cust->orders;
+
+	// guaranteed that the list is not empty by the time we get here
+	
+	// if it's the first item in the list we're getting rid of
+	if (curr->order_num == order_num) {
 		temp = cust->orders->next;
 		free(cust->orders);
+		printf("I deleted something at the front of the list.\n");
 		cust->orders = temp;
-		if (temp != NULL) {
-			printf("Next order in line is #%d.\n", cust->orders->order_num);
-		}
-	} else {
-		curr = cust->orders;
-		if (curr->order_num == order_num) {
-			printf("Deleting order #%d.\n", order_num);
-			temp = curr->next;
-			free(curr);
-			cust->orders = temp;
-			if (temp != NULL) {
-				printf("Next in line is #%d.\n", cust->orders->order_num);
+	} else { // if it's anywhere else in the list
+		while (curr->next != NULL) {
+			if (curr->next->order_num == order_num) {
+				temp = curr->next->next;
+				free(curr->next);
+				printf("I deleted something within/at end of list.\n");
+				curr->next = temp;
 			}
-		} else {
-			while (curr->next != NULL) {
-				if (curr->order_num == order_num) {
-					printf("Deleting order #%d.\n", order_num);
-					temp = curr->next->next;
-					free(curr->next);
-					curr->next = temp;
-					printf("Next is #%d.\n", temp->order_num);
-					break;
-				}
 			curr = curr->next;
-			}
 		}
 	}
-		
+	
 	*revenue = cash;
 	return 1;
 }
